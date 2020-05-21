bb093dcf7942d2b88300e87b6e8dad7acd56ee05
Sara Cunningham <59973201+saracunn@users.noreply.github.com>
2020-05-20 21:57:02 -0400

    Create BusinessCard.js

---
+++ BusinessCard.js
@@ -0,0 +1,56 @@
+import { LitElement, html, css } from 'lit-element';
+
+class BusinessCard extends LitElement {
+    // defined css styling
+    static get styles() {
+        return css`
+            .card{
+                box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+                transition: 0.3s;
+                width: 300px;
+            }
+            .card:hover{
+                box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
+            }
+            .info{
+                padding: 2px 16px;
+            }
+        `;
+    }
+
+    render() {
+        return html`
+            <div class="card">
+                <img src="${this.img}" width=100%>
+            <div class="info">
+                <h3><b> ${this.name}</b> </h3>
+                <p><slot name = "College"></slot></p>
+                <p><slot name = "Major"></slot></p>
+                <p> <a href="${this.url}"> Website </a></p>
+                </div>
+            </div>
+        `;
+    }
+
+    constructor() {
+        super();
+        this.name = "Person's name";
+        this.url = "";
+        this.img = "";
+        this.college ="";
+        this.major = "";
+    }
+
+    static get properties() {
+        return {
+            name: { type: String },
+            url: { type: String },
+            img: { type: String },
+            college: { type: String},
+            major: { type: String}
+        };
+    }
+
+}
+
+customElements.define('business-card', BusinessCard);
